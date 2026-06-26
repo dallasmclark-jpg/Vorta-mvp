0f51f208abe6fb24322ec6356183892d222af0f5
dallasmclark-jpg <dallasmclark@gmail.com>
2026-06-26 19:50:46 +0100

    Remove StrictMode to fix flashing

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,10 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import "../tailwind.css";
 import { AiOperations } from "./screens/AiOperations";
 
 createRoot(document.getElementById("app") as HTMLElement).render(
-  <StrictMode>
-    <BrowserRouter>
-      <AiOperations />
-    </BrowserRouter>
-  </StrictMode>,
+  <BrowserRouter>
+    <AiOperations />
+  </BrowserRouter>,
 );
