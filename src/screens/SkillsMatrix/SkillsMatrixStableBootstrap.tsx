bf2afe72ff68df18a0959a79af90bb4b0577b648
dallasmclark-jpg <dallasmclark@gmail.com>
2026-07-17 16:31:30 +0100

    Activate stable Skills Matrix experience

--- src/screens/SkillsMatrix/SkillsMatrixStableBootstrap.tsx
+++ src/screens/SkillsMatrix/SkillsMatrixStableBootstrap.tsx
@@ -1,5 +1,19 @@
+import { useEffect } from "react";
+import { supabase } from "../../lib/supabaseClient";
 import { SkillsMatrixSection as SkillsMatrixSelectionExperience } from "./SkillsMatrixSelectionExperience";
 
-export const SkillsMatrixSection = (): JSX.Element => (
-  <SkillsMatrixSelectionExperience />
-);
+const SKILLS_MATRIX_FUNCTION = "skills-matrix-data";
+
+export const SkillsMatrixSection = (): JSX.Element => {
+  useEffect(() => {
+    const refreshPayload = window.setTimeout(() => {
+      void supabase.functions.invoke(SKILLS_MATRIX_FUNCTION, {
+        body: { schemaVersion: "capability-v3" },
+      });
+    }, 100);
+
+    return () => window.clearTimeout(refreshPayload);
+  }, []);
+
+  return <SkillsMatrixSelectionExperience />;
+};
