6dcd9e813c0fa38f6699ffa7f6c50b4def3c2235
dallasmclark-jpg <dallasmclark@gmail.com>
2026-07-23 20:19:13 +0100

    feat: mount mobile risk scope selector

--- src/screens/AiOperations/sections/DashboardOverviewSection/index.ts
+++ src/screens/AiOperations/sections/DashboardOverviewSection/index.ts
@@ -1,1 +1,22 @@
-export { DashboardOverviewSection } from "./DashboardOverviewSection";
+import {
+  Fragment,
+  createElement,
+} from "react";
+import {
+  DashboardOverviewSection as DashboardOverviewSectionBase,
+} from "./DashboardOverviewSection";
+import {
+  MobileRiskScopeSelector,
+} from "./MobileRiskScopeSelector";
+
+export const DashboardOverviewSection = (): JSX.Element =>
+  createElement(
+    Fragment,
+    null,
+    createElement(
+      DashboardOverviewSectionBase,
+    ),
+    createElement(
+      MobileRiskScopeSelector,
+    ),
+  );
