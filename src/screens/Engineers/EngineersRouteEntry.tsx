5bc5fa443655a16c76cab7cb0519770796f43404
dallasmclark-jpg <dallasmclark@gmail.com>
2026-07-24 08:20:58 +0100

    Route phones to mobile Engineers workflow

--- src/screens/Engineers/EngineersRouteEntry.tsx
+++ src/screens/Engineers/EngineersRouteEntry.tsx
@@ -1,15 +1,24 @@
 import { useAuth } from "../../lib/auth";
 import { getEffectiveDataMode } from "../../lib/dataTrust";
+import { useMediaQuery } from "../../hooks/useMediaQuery";
 import { EngineersSection as DemoEngineersSection } from "./EngineersSection";
 import { LiveEngineersSection } from "./LiveEngineersSection";
+import { MobileEngineersSection } from "./MobileEngineersSection";
 
 export function EngineersRouteEntry(): JSX.Element {
   const { siteContext } = useAuth();
   const dataMode = getEffectiveDataMode(Boolean(siteContext?.siteId));
+  const isPhone = useMediaQuery("(max-width: 639px)");
 
   return (
     <div className="contents" data-vorta-engineers-mode={dataMode}>
-      {dataMode === "demo" ? <DemoEngineersSection /> : <LiveEngineersSection />}
+      {isPhone ? (
+        <MobileEngineersSection dataMode={dataMode} />
+      ) : dataMode === "demo" ? (
+        <DemoEngineersSection />
+      ) : (
+        <LiveEngineersSection />
+      )}
     </div>
   );
 }
