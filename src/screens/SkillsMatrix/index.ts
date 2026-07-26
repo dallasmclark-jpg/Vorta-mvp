911a9d090171f6ec8b01010c558b3e8b498be46d
dallasmclark-jpg <dallasmclark@gmail.com>
2026-07-26 17:07:53 +0100

    Use capability summary across phone widths

--- src/screens/SkillsMatrix/index.ts
+++ src/screens/SkillsMatrix/index.ts
@@ -1,1 +1,15 @@
-export { SkillsMatrixSection } from "./SkillsMatrixNative";
+import { useMediaQuery } from "../../hooks/useMediaQuery";
+import { useAuth } from "../../lib/auth";
+import { getEffectiveDataMode } from "../../lib/dataTrust";
+import { MobileCapabilitySummary } from "./MobileCapabilitySummary";
+import { SkillsMatrixSection as NativeSkillsMatrixSection } from "./SkillsMatrixNative";
+
+export function SkillsMatrixSection(): JSX.Element {
+  const { siteContext } = useAuth();
+  const dataMode = getEffectiveDataMode(Boolean(siteContext?.siteId));
+  const isPhone = useMediaQuery("(max-width: 767px)");
+
+  return isPhone
+    ? <MobileCapabilitySummary dataMode={dataMode} />
+    : <NativeSkillsMatrixSection />;
+}
