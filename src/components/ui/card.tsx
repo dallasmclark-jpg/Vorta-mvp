d4d0c2291906c74b816e65d50d974a18052f7005
dallasmclark-jpg <dallasmclark@gmail.com>
2026-07-27 19:35:02 +0100

    Standardise shared Vorta card styling

--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -7,8 +7,9 @@ const Card = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <div
     ref={ref}
+    data-vorta-card="true"
     className={cn(
-      "rounded-xl border bg-card text-card-foreground shadow",
+      "vorta-card rounded-2xl border bg-card text-card-foreground",
       className,
     )}
     {...props}
@@ -22,7 +23,10 @@ const CardHeader = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <div
     ref={ref}
-    className={cn("flex flex-col space-y-1.5 p-6", className)}
+    className={cn(
+      "flex flex-col space-y-1.5 p-4 sm:p-5 lg:p-6",
+      className,
+    )}
     {...props}
   />
 ));
@@ -56,7 +60,14 @@ const CardContent = React.forwardRef<
   HTMLDivElement,
   React.HTMLAttributes<HTMLDivElement>
 >(({ className, ...props }, ref) => (
-  <div ref={ref} className={cn("p-6 pt-0", className)} {...props} />
+  <div
+    ref={ref}
+    className={cn(
+      "p-4 pt-0 sm:p-5 sm:pt-0 lg:p-6 lg:pt-0",
+      className,
+    )}
+    {...props}
+  />
 ));
 CardContent.displayName = "CardContent";
 
@@ -66,7 +77,10 @@ const CardFooter = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <div
     ref={ref}
-    className={cn("flex items-center p-6 pt-0", className)}
+    className={cn(
+      "flex items-center p-4 pt-0 sm:p-5 sm:pt-0 lg:p-6 lg:pt-0",
+      className,
+    )}
     {...props}
   />
 ));
