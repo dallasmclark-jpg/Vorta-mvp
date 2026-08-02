7fdd1610ac992e3b651c85bed4284944f00dade4
dallasmclark-jpg <dallasmclark@gmail.com>
2026-08-02 09:19:24 +0100

    VOR-035: isolate tablet engineers without replacing desktop

--- src/screens/Engineers/EngineersRouteEntry.tsx
+++ src/screens/Engineers/EngineersRouteEntry.tsx
@@ -1,6 +1,7 @@
 import { useMediaQuery } from "../../hooks/useMediaQuery";
 import { useAuth } from "../../lib/auth";
 import { getEffectiveDataMode } from "../../lib/dataTrust";
+import { LiveEngineersSection } from "./LiveEngineersSection";
 import { MobileEngineersSection } from "./MobileEngineersSection";
 import { TabletEngineersSection } from "./TabletEngineersSection";
 
@@ -8,13 +9,16 @@ export function EngineersRouteEntry(): JSX.Element {
   const { siteContext } = useAuth();
   const dataMode = getEffectiveDataMode(Boolean(siteContext?.siteId));
   const isPhone = useMediaQuery("(max-width: 767px)");
+  const isTablet = useMediaQuery("(min-width: 768px) and (max-width: 1439px)");
 
   return (
     <div className="contents" data-vorta-engineers-mode={dataMode}>
       {isPhone ? (
         <MobileEngineersSection dataMode={dataMode} />
-      ) : (
+      ) : isTablet ? (
         <TabletEngineersSection />
+      ) : (
+        <LiveEngineersSection />
       )}
     </div>
   );
